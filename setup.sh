70fc29f8014eae3faedb31dcca17a9658ecaf6cc
Sheridan Duff <sfduff@users.noreply.github.com>
2021-08-08 11:55:16 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # install prerequisites
-sudo apt-get install i2c-tools python3-pip git -y
-pip3 install RPi.bme280 mh-z19 smbus2 paho-mqtt
+sudo apt-get install i2c-tools python3-pip -y
+sudo pip3 install RPi.bme280 mh-z19 smbus2 paho-mqtt
 
 # allow access to serial port for non root
 sudo chmod 777 /dev/serial0

6e33b76c9852e021fe620da3526784b43cb52724
sfduff <sfduff@mac.com>
2021-08-08 03:42:07 +0100

    added setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# install prerequisites
+sudo apt-get install i2c-tools python3-pip git -y
+sudo pip3 install RPi.bme280 mh-z19 smbus2 paho-mqtt
+
+# allow access to serial port for non root
+sudo chmod 777 /dev/serial0
+
+# setup sensor service
+sudo cp sensors.service /lib/systemd/system/.
+sudo systemctl daemon-reload
+sudo systemctl start sensors.service
+sudo systemctl status sensors.service
